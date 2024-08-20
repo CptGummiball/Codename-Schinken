56c5c714739ef3bd74e7438b71a39bcf2f8af1b4
CaptainGummiball <151415484+CptGummiball@users.noreply.github.com>
2024-08-20 22:11:37 +0200

    Add files via upload

---
+++ database-test/firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDPHiz4afwH5rUOr5AFcAsAkEFDcDdYiAw",
+    authDomain: "codename-schinken-database.firebaseapp.com",
+    databaseURL: "https://codename-schinken-database-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "codename-schinken-database",
+    storageBucket: "codename-schinken-database.appspot.com",
+    messagingSenderId: "616956346619",
+    appId: "1:616956346619:web:0a736705bdaf9dde41705e",
+    measurementId: "G-PE162WR6G8"
+};
+
+export default firebaseConfig;
